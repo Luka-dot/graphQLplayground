eb14921dd7ddd87efba3809207971896cce573a9
Luka-dot <79lukask@gmail.com>
2020-12-26 17:54:36 -0500

    graphql yoga added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Started project file index.js')
+import { message } from './myModule';
+import { adding, substracting } from './math';
+
+console.log(message);
+
+console.log(adding(2, 2));
+substracting( 10, 5);
