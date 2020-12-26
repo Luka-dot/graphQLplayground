b136abff8c5f1e174883d444376d6e03737107d0
Luka-dot <79lukask@gmail.com>
2020-12-26 18:06:24 -0500

    simple basic queries and resolvers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
-import { message } from './myModule';
-import { adding, substracting } from './math';
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(message);
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+    }
+`
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first querry'
+        },
+        name() {
+            return "Lukas"
+        },
+        location() {
+            return 'Clayton NC'
+        }
+    }
+}
 
-console.log(adding(2, 2));
-substracting( 10, 5);
+const server = new GraphQLServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+})
+
+server.start(() => {
+    console.log('Server is running!!!')
+})
+
+
+
+
+
+
+// import { message } from './myModule';
+// import { adding, substracting } from './math';
+
+// console.log(message);
+
+// console.log(adding(2, 2));
+// substracting( 10, 5);
