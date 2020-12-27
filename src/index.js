2a6b73937604771f1d8160ef6d59a9fc073ed657
Luka-dot <79lukask@gmail.com>
2020-12-27 11:27:47 -0500

    query on custom types

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// temp user data before database set up
+const users = [{
+    id: 1,
+    name: "Lukas",
+    email: "lukas@gma.com",
+    age: 39
+},
+{
+    id: 2,
+    name: "Sara",
+    email: "Sara@gma.com",
+    age: 29
+},
+{
+    id: 3,
+    name: "Mikey",
+    email: "mouse@gma.com"
+}]
+
+const posts = [{
+    id: 1,
+    title: "post one",
+    body: "lukas@gma.com",
+    published: true
+},
+{
+    id: 2,
+    title: "post TWO",
+    body: "random text",
+    published: true
+},
+{
+    id: 3,
+    title: "post THREE",
+    body: "More and more of random text",
+    published: false
+}]
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        greeting(name: String, position: String): String!
-        add(numberOne: Float!, numberTwo: Float!): Float
-        addGrades(numbers: [Float!]!): Float!
-        grades: [Int!]!
+        users(query: String): [User!]!
         me:  User!
         post: Post!
+        posts(query: String): [Post!]!
     }
     
     type User {
@@ -28,27 +64,23 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
     Query: {
-        add(parent, args, ctx, info) {
-            return args.numberOne + args.numberTwo
-        },
-        addGrades(parent, args, ctx, info) {
-            if (args.numbers.length === 0) {
-                return 0
-            } else {
-                return args.numbers.reduce((accumulator, currentValue) => {
-                    return accumulator + currentValue
-                })
+        posts(parent,args, ctx, info) {
+            if (!args.query) {
+                return posts
             }
+
+            return posts.filter((post) => {
+                return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
+            })
         },
-        greeting(parent, args, ctx, info) {
-            if (!args.name) {
-                return 'Hello user.'
-            } else {
-            return `Hello ${args.name}, you are my favorite ${args.position}`
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
             }
-        },
-        grades(parent, args, ctx, info) {
-            return [98, 55, 79]
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
         },
         me() {
             return {
@@ -137,4 +169,82 @@ server.start(() => {
 
 // server.start(() => {
 //     console.log('Server is running!!!')
+// })
+
+
+
+
+// // Type definitions (schema)
+// const typeDefs = `
+//     type Query {
+//         greeting(name: String, position: String): String!
+//         add(numberOne: Float!, numberTwo: Float!): Float
+//         addGrades(numbers: [Float!]!): Float!
+//         grades: [Int!]!
+//         me:  User!
+//         post: Post!
+//     }
+    
+//     type User {
+//         id: ID!
+//         name: String!
+//         email: String!
+//         age: Int
+//     }
+    
+//     type Post {
+//         id: ID!
+//         title: String!
+//         body: String!
+//         published: Boolean!
+//     } 
+// `
+// // Resolvers
+// const resolvers = {
+//     Query: {
+//         add(parent, args, ctx, info) {
+//             return args.numberOne + args.numberTwo
+//         },
+//         addGrades(parent, args, ctx, info) {
+//             if (args.numbers.length === 0) {
+//                 return 0
+//             } else {
+//                 return args.numbers.reduce((accumulator, currentValue) => {
+//                     return accumulator + currentValue
+//                 })
+//             }
+//         },
+//         greeting(parent, args, ctx, info) {
+//             if (!args.name) {
+//                 return 'Hello user.'
+//             } else {
+//             return `Hello ${args.name}, you are my favorite ${args.position}`
+//             }
+//         },
+//         grades(parent, args, ctx, info) {
+//             return [98, 55, 79]
+//         },
+//         me() {
+//             return {
+//                 id: '123345678',
+//                 name: 'Mike',
+//                 email: 'mike@gmail.com',
+//                 age: 28
+//             }
+//         },
+
+//         post() {
+//             return {
+//                 id: '1233098',
+//                 title: 'First post',
+//                 body: 'This is a post to my new grapQL API',
+//                 published: true
+//             }
+//         }
+//     }
+// }
+
+// const server = new GraphQLServer({
+//     typeDefs: typeDefs,
+//     resolvers: resolvers
 // })
