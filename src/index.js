e5e2c8042b1aff446bc6933c3040c6c0e6790985
Luka-dot <79lukask@gmail.com>
2020-12-26 19:26:20 -0500

    operational arguments

--- src/index.js
+++ src/index.js
@@ -3,30 +3,51 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definitions (schema)
 const typeDefs = `
     type Query {
+        greeting(name: String, position: String): String!
+        me:  User!
+        post: Post!
+    }
+    
+    type User {
         id: ID!
         name: String!
-        age: Int!
-        employed: Boolean!
-        gpa: Float                 
+        email: String!
+        age: Int
     }
+    
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    } 
 `
 // Resolvers
 const resolvers = {
     Query: {
-        id() {
-            return 'ZXT234'
+        greeting(parent, args, ctx, info) {
+            if (!args.name) {
+                return 'Hello user.'
+            } else {
+            return `Hello ${args.name}, you are my favorite ${args.position}`
+            }
         },
-        name() {
-            return 'Lukas'
+        me() {
+            return {
+                id: '123345678',
+                name: 'Mike',
+                email: 'mike@gmail.com',
+                age: 28
+            }
         },
-        age() {
-            return 27
-        },
-        employed() {
-            return true
-        },
-        gpa() {
-            return 3.77  // can return null since we did not define type with !
+
+        post() {
+            return {
+                id: '1233098',
+                title: 'First post',
+                body: 'This is a post to my new grapQL API',
+                published: true
+            }
         }
     }
 }
@@ -52,3 +73,50 @@ server.start(() => {
 
 // console.log(adding(2, 2));
 // substracting( 10, 5);
+
+// // Type definitions (schema)
+// const typeDefs = `
+//     type Query {
+//         id: ID!
+//         name: String!
+//         age: Int!
+//         employed: Boolean!
+//         gpa: Float                 
+//     }
+    
+//     type User {
+//         id: ID!
+//         name: String!
+//         email: String!
+//         age: Int
+//     } 
+// `
+// // Resolvers
+// const resolvers = {
+//     Query: {
+//         id() {
+//             return 'ZXT234'
+//         },
+//         name() {
+//             return 'Lukas'
+//         },
+//         age() {
+//             return 27
+//         },
+//         employed() {
+//             return true
+//         },
+//         gpa() {
+//             return 3.77  // can return null since we did not define type with !
+//         }
+//     }
+// }
+
+// const server = new GraphQLServer({
+//     typeDefs: typeDefs,
+//     resolvers: resolvers
+// })
+
+// server.start(() => {
+//     console.log('Server is running!!!')
+// })
